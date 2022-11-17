4f2abefe0f1e3d76410c27ed6c5927bd1bfcb74e
Susanna02 <74212584+Susanna02@users.noreply.github.com>
2022-11-17 11:38:16 +0100

    conversation added

--- TwoWorlds/Assets/NpcChatter.cs
+++ TwoWorlds/Assets/NpcChatter.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class NpcChatter : MonoBehaviour //needs own tag for player as trigger
@@ -9,38 +8,74 @@ public class NpcChatter : MonoBehaviour //needs own tag for player as trigger
     public List<string> comments;
     string previousComment;
     int chosenComment;
+    int chosenConversation;
+
+    public List<Conversation> conversations;
+    bool conversationUsed = false;
+    Conversation previousConversation;
 
     public Transform bubbleSpawn;
-    public GameObject speechBubble;
-    public GameObject canvas;
-    GameObject bubble;
-    //public Text commentText;
+    public GameObject speechBubble; //Prefab
 
-    public TextMeshProUGUI chatterText;
+    public GameObject canvas;
+    public GameObject canvas2;
+
+    GameObject bubble;
+    bool bubbleTurn = false;
+
+    TextMeshProUGUI chatterText;
+    TextMeshProUGUI chatterText2;
 
     private void Start()
     {
+        chatterText = canvas.GetComponentInChildren<TextMeshProUGUI>();
+        chatterText2 = canvas2.GetComponentInChildren<TextMeshProUGUI>();
+
         chatterText.enabled = false;
+        chatterText2.enabled = false;
     }
 
     public void ChooseComment()
     {
-        int max = comments.Count - 1;
-        chosenComment = Random.Range(0, max);
+        if (conversations.Count < 1 || conversationUsed == true)
+        {
+            int max = comments.Count - 1;
+            chosenComment = Random.Range(0, max);
 
-        if (comments[chosenComment] != previousComment)
-            ShowComment();
-        //else ChooseComment(); // geht das? oder stattdessen while-loop?
+            if (comments[chosenComment] != previousComment)
+                ShowComment();
+            //else ChooseComment(); // geht das? oder stattdessen while-loop?
+        }
+        else // npc conversation
+        {
+            int max = conversations.Count - 1;
+            chosenConversation = Random.Range(0, max);
+            // check if already used chosenConversation
+            if (conversationUsed == false) StartCoroutine(ShowNpcConversation());
+        }
+        
     }
 
     void ShowComment()
     {
-        bubble = Instantiate(speechBubble, canvas.transform.position, Quaternion.identity);
-
+        if (bubbleTurn == false) bubble = Instantiate(speechBubble, canvas.transform.position, Quaternion.identity);
+        else bubble = Instantiate(speechBubble, canvas2.transform.position, Quaternion.identity);
         bubble.transform.localScale += new Vector3(canvas.transform.localScale.x * 0.8f, canvas.transform.localScale.y * 0.8f, 0);
 
-        chatterText.text = comments[chosenComment];
-        chatterText.enabled = true;
+        
+
+        if (bubbleTurn == false)
+        {
+            if (conversations.Count < 1) chatterText.text = comments[chosenComment];
+            else chatterText.text = conversations[chosenConversation].conversation[chosenComment];
+            chatterText.enabled = true;
+        } 
+        else 
+        {
+            if (conversations.Count < 1) chatterText2.text = comments[chosenComment];
+            else chatterText2.text = conversations[chosenConversation].conversation[chosenComment];
+            chatterText2.enabled = true;
+        }
         speechBubble.SetActive(true);
     }
 
@@ -49,11 +84,25 @@ public class NpcChatter : MonoBehaviour //needs own tag for player as trigger
         previousComment = comments[chosenComment];
         Destroy(bubble);
         chatterText.text = "";
+        chatterText2.text = "";
+        chatterText.enabled = false;
+        chatterText2.enabled = false;
     }
 
     IEnumerator ShowNpcConversation()
     {
-        yield return new WaitForSeconds(1);
-        
+        //conversationUsed = true;
+        int selection = 0;
+        foreach(string text in conversations[chosenConversation].conversation)
+        {
+            chosenComment = selection;
+            ShowComment();
+            yield return new WaitForSeconds(2f);
+            HideComment();
+            selection++;
+            if (bubbleTurn == false) bubbleTurn = true;
+            else bubbleTurn = false;
+        }
+        conversations[chosenConversation] = previousConversation;
     }
 }
